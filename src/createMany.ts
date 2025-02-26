8482e8bfe45a9d38abc284d20a381523de6dff57
Md Abdur Razzak <rakibt23p@gmail.com>
2025-02-26 09:52:20 +0600

    update create user

--- src/createMany.ts
+++ src/createMany.ts
@@ -1,143 +1,34 @@
-import { PrismaClient } from "@prisma/client";
+import {PrismaClient, UserRole} from "@prisma/client";
 
 const prisma = new PrismaClient()
 
 
-const mainFunction = async () => {
-    const createMany = await prisma.post.createMany({
-        data: [{
-            titlle: "Post 1", // Fixed property name from 'title' to 'titlle'
-            content: "Content 1",
-            author: "Author name"
-        },
-        {
-            titlle: "Post 2", // Fixed property name from 'title' to 'titlle'
-            content: "Content 2",
-            author: "Author name"
-        },
-        {
-            titlle: "Post 3", // Fixed property name from 'title' to 'titlle'
-            content: "Content 3",
-            author: "Author name"
-        },
-        {
-            titlle: "Post 4", // Fixed property name from 'title' to 'titlle'
-            content: "Content 4",
-            author: "Author name"
-        },
-        {
-            titlle: "Post 5", // Fixed property name from 'title' to 'titlle'
-            content: "Content 5",
-            author: "Author name"
-        },
-        {
-            titlle: "Post 6", // Fixed property name from 'title' to 'titlle'
-            content: "Content 6",
-            author: "Author name"
-        },
-        {
-            titlle: "Post 7", // Fixed property name from 'title' to 'titlle'
-            content: "Content 7",
-            author: "Author name"
-        },
-        {
-            titlle: "Post 8", // Fixed property name from 'title' to 'titlle'
-            content: "Content 8",
-            author: "Author name"
-        },
-        {
-            titlle: "Post 9", // Fixed property name from 'title' to 'titlle'
-            content: "Content 9",
-            author: "Author name"
-        },
-        {
-            titlle: "Post 10", // Fixed property name from 'title' to 'titlle'
-            content: "Content 10",
-            author: "Author name"
-        },
-        {
-            titlle: "Post 11", // Fixed property name from 'title' to 'titlle'
-            content: "Content 11",
-            author: "Author name"
+const  main = async ()=>{
+//     create user
+const  createUser = await prisma.user.create({
+    data:{
+        userName:"Md Abdur Razzak",
+        email:"mdabdurrazzakrakib290@gmail.com",
+        role:UserRole.ADMIN,
 
-        },
-        {
-            titlle: "Post 12", // Fixed property name from 'title' to 'titlle'
-            content: "Content 12",
-            author: "Author name"
+    }
 
-        },
-        {
-            titlle: "Post 13", // Fixed property name from 'title' to 'titlle'
-            content: "Content 13",
-            author: "Author name"
-
-        },
-        {
-            titlle: "Post 14", // Fixed property name from 'title' to 'titlle'
-            content: "Content 14",
-            author: "Author name"
-
-        },
-        {
-            titlle: "Post 15", // Fixed property name from 'title' to 'titlle'
-            content: "Content 15",
-            author: "Author name"
-
-        },
-        {
-            titlle: "Post 16", // Fixed property name from 'title' to 'titlle'
-            content: "Content 16",
-            author: "Author name"
-
-        },
-        {
-            titlle: "Post 17", // Fixed property name from 'title' to 'titlle'
-            content: "Content 17",
-            author: "Author name"
-
-        },
-        {
-            titlle: "Post 18", // Fixed property name from 'title' to 'titlle'
-            content: "Content 18",
-            author: "Author name"
-        },
-        {
-            titlle: "Post 19", // Fixed property name from 'title' to 'titlle'
-            content: "Content 19",
-            author: "Author name"
-        },
-        {
-            titlle: "Post 20", // Fixed property name from 'title' to 'titlle'
-            content: "Content 20",
-            author: "Author name"
-        },
-        {
-            titlle: "Post 21", // Fixed property name from 'title' to 'titlle'
-            content: "Content 21",
-            author: "Author name"
-        },
-        {
-            titlle: "Post 22", // Fixed property name from 'title' to 'titlle'
-            content: "Content 22",
-            author: "Author name"
-        },
-        {
-            titlle: "Post 23", // Fixed property name from 'title' to 'titlle'
-            content: "Content 23",
-            author: "Author name"
-        },
-        {
-            titlle: "Post 24", // Fixed property name from 'title' to 'titlle'
-            content: "Content 24",
-            author: "Author name"
-        },
-
-        ]
+})
+//     console.log(createUser)
+//     create profile
+    const  createProfile = await prisma.profile.create({
+        data:{
+            bio:"Hey I'm Rakib I work as a Backend Developer",
+            userId:2
+        }
     })
-    console.log(createMany);
+    const  createCategory = await prisma.category.create({
+        data:{
+            name:"Next Label Web Developer."
+        }
+    })
 
 
+    console.log(createCategory)
 }
-
-mainFunction()
+main()
